84011d22190510ac1a1f2c144c773b047e2406c1
gylim0604 <gylim0604@hotmail.com>
2020-10-06 22:07:37 -0500

    added gale_shapely test cases

---
+++ gale-shapely/test_gale_shapely.py
@@ -0,0 +1,16 @@
+import unittest
+from gale_shapely import gale_shapely
+
+class TestGale_Shapely(unittest.TestCase):
+
+    def test_one_to_one(self):
+        men = {'a':{'A'}}
+        women = {'A':{'a'}}
+        result = [['a','A']]
+        self.assertEqual(gale_shapely(men, women), result)
+
+    def test_three_to_three(self):
+        men = {'a':{'A','B','C'}, 'b':{'B','C','A'}, 'c':{'C','A','B'}}
+        women = {'A':{'b','c','a'}, 'B':{'c','b','a'}, 'C':{'a','b','c'}}
+        result = [['a','A'],['b','B'],['c','C']]
+        self.assertEqual(gale_shapely(men,women), result)
